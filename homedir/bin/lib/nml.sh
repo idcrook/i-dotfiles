4a0b47f1a03e1b92ced17dd8f468c8368f07ec30
David Crook <idcrook@users.noreply.github.com>
2019-05-06 19:13:47 -0600

    add usage example

--- homedir/bin/lib/nml.sh
+++ homedir/bin/lib/nml.sh
@@ -1,6 +1,7 @@
 #!/bin/echo Error: sourcing -*- sh -*-
 
 # (SUDO) nmap and lists IP => MAC addr (vendor name) for an NMAP range
+# ex. nml '10.0.1.*'
 function nml() {
    sudo nmap -sn "$@" | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "substr($0, index($0,$3)) }' | sort --version-sort
  }
