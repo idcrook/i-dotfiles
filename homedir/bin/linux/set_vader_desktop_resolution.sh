3e038945a1cb71bab8e47fe87ed236ce094416c3
David Crook <idcrook@users.noreply.github.com>
2020-03-27 15:50:28 -0600

    update DP port and add note in comment

--- homedir/bin/linux/set_vader_desktop_resolution.sh
+++ homedir/bin/linux/set_vader_desktop_resolution.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
-nvidia-settings --assign CurrentMetaMode="HDMI-0: nvidia-auto-select +2560+0 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}, DP-0: nvidia-auto-select +0+0 {ViewPortIn=2560x1440, ViewPortOut=3840x2160+0+0}"
+# DP-0 solo port in second row
+# DP-2 single port between HDMI and top of card
+nvidia-settings --assign CurrentMetaMode="HDMI-0: nvidia-auto-select +2560+0 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}, DP-2: nvidia-auto-select +0+0 {ViewPortIn=2560x1440, ViewPortOut=3840x2160+0+0}"
 
 # for some reason the X server does this instead, offsetting the viewport for the second screen (screen on  the right):
 # Setting mode "HDMI-0: nvidia-auto-select @2560x1440 +3840+0 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}, DP-0: nvidia-auto-select @2560x1440 +0+0 {ViewPortIn=2560x1440, ViewPortOut=3840x2160+0+0}"
