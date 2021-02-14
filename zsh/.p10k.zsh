f3545c93cfb398507e943e81ec7d06eee1ef2df0
David Crook <idcrook@users.noreply.github.com>
2021-02-14 11:27:18 -0700

    turn off cpuload and free ram in right-side of prompt

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -90,9 +90,9 @@
     midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vpn_ip                # virtual private network indicator
-    load                  # CPU load
+    # load                  # CPU load
     # disk_usage            # disk usage
-    ram                   # free RAM
+    # ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
