fe1a47f1dfe6976ec0588cac4fa3571fa1e18c21
David Crook <idcrook@users.noreply.github.com>
2021-04-08 10:52:31 -0600

    add esp8266 RTOS SDK

--- shell/.config/shell/env.ANY-linux.sh
+++ shell/.config/shell/env.ANY-linux.sh
@@ -10,6 +10,16 @@
 # stow -vv shell
 #
 
+# esp8266 rtos SDK
+if [[ -d "${HOME}"/projects/hw/esp/xtensa-lx106-elf/bin ]] ; then
+    export PATH="$PATH:$HOME/projects/hw/esp/xtensa-lx106-elf/bin"
+fi
+
+# Local Variables:
+# mode: sh
+# eval: (sh-set-shell "bash")
+# End:
+
 
 # Local Variables:
 # mode: sh
