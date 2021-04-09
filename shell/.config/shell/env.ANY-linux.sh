d04531d7cfdb26c0b471643dda778e05923d5f01
David Crook <idcrook@users.noreply.github.com>
2021-04-08 19:15:31 -0600

    add ESP8266 SDK stuck (and sync .emacs.d)

--- shell/.config/shell/env.ANY-linux.sh
+++ shell/.config/shell/env.ANY-linux.sh
@@ -15,6 +15,19 @@ if [[ -d "${HOME}"/projects/hw/esp/xtensa-lx106-elf/bin ]] ; then
     export PATH="$PATH:$HOME/projects/hw/esp/xtensa-lx106-elf/bin"
 fi
 
+# IDF_PATH = Espressif SDK
+
+# # ESP-IDF - FIXME: create an alias for switching to ESP32
+# if [[ -d "${HOME}/projects/hw/esp/esp-idf" ]] ; then
+#     export IDF_PATH="${HOME}/projects/hw/esp/esp-idf"
+# fi
+
+# ESP8266_RTOS_SDK
+if [[ -d "${HOME}/projects/hw/esp/ESP8266_RTOS_SDK" ]] ; then
+    export IDF_PATH="${HOME}/projects/hw/esp/ESP8266_RTOS_SDK"
+fi
+
+
 # Local Variables:
 # mode: sh
 # eval: (sh-set-shell "bash")
