e9c8b7a43c14704d6b6750eecd35f539b7bd2e69
David Crook <idcrook@users.noreply.github.com>
2021-12-23 16:05:52 -0700

    fix check for already installed for selected apps

--- @windows/install_3dprint.ps1
+++ @windows/install_3dprint.ps1
@@ -1,15 +1,19 @@
 # Borrowed code from https://gist.github.com/Codebytes/29bf18015f6e93fca9421df73c6e512c
 #Install New apps
-Write-Output "Installing Apps"
 $apps = @(
     @{name = "OpenSCAD" }, 
-    @{name = "OpenJSCAD"; source = "msstore" }, 
     @{name = "Ultimaker.Cura" }, 
     @{name = "FreeCAD.FreeCAD" }, 
     @{name = "KiCad.KiCad" }, 
     @{name = "Prusa3D.PrusaSlicer" }
 );
 
+$msstoreapps = @(
+    @{name = "OpenJSCAD" }
+);
+
+
+Write-Output "Installing Apps"
 Foreach ($app in $apps) {
     $listApp = winget list --exact -q $app.name
     if (![String]::Join("", $listApp).Contains($app.name)) {
@@ -19,6 +23,23 @@ Foreach ($app in $apps) {
         }
         else {
             winget install --exact --silent $app.name 
+        }
+    }
+    else {
+        Write-host "Skipping Install of " $app.name
+    }
+}
+
+Write-Output "Installing MSSTORE Apps"
+Foreach ($app in $msstoreapps) {
+    $listApp = winget list --exact -q $app.name
+    if (![String]::Join("", $listApp).Contains($app.name)) {
+        Write-host "Installing:" $app.name
+        if ($app.source -ne $null) {
+            winget install --exact --silent $app.name --source $app.source
+        }
+        else {
+            winget install --silent $app.name --source msstore
         }
     }
     else {
