c39fc2299f76657424f99db8e2ab621ef8c195bc
David Crook <idcrook@users.noreply.github.com>
2022-01-25 15:15:48 -0700

    add miniconda3 to KYLO (WSL2)

---
+++ shell/.config/shell/env.KYLO.sh
@@ -0,0 +1,39 @@
+# -*- sh -*-
+#
+# per-host environment variable settings
+#
+# - `hostname` in filename should be replaced by hostname (`uname -n` nodename)
+# - should be sourceable by both bash or zsh
+#
+# INSTALL AFTER CREATION
+#
+# cd ~/.dotfiles
+# stow -vv shell
+#
+# EXAMPLE
+#
+# # to use the shared openscad-mcad in openscad-nightly on ubuntu, need to update
+# # an environment variable
+# if [ -d /usr/share/openscad/libraries ] ; then
+#     export OPENSCADPATH=/usr/share/openscad/libraries:"${OPENSCADPATH}"
+# fi
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/dpc/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/dpc/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/dpc/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/dpc/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Local Variables:
+# mode: sh
+# eval: (sh-set-shell "bash")
+# End:
