4d8304d86eb24e1f266ab73031e7a7854c2c33b2
David Crook <idcrook@users.noreply.github.com>
2024-01-28 10:42:54 -0700

    use more standard  path for RasPi Pico SDK

--- shell/.config/shell/env.ANY-any.sh
+++ shell/.config/shell/env.ANY-any.sh
@@ -14,30 +14,30 @@
 # - https://github.com/raspberrypi/pico-sdk
 # - https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf
 # - https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.sh
-# export PICO_SDK_PATH=/home/pi/projects/pico/pico-sdk
-# export PICO_EXAMPLES_PATH=/home/pi/projects/pico/pico-examples
-# export PICO_EXTRAS_PATH=/home/pi/projects/pico/pico-extras
-# export PICO_PLAYGROUND_PATH=/home/pi/projects/pico/pico-playground
-if [[ -d "${HOME}"/projects/pico/pico-sdk ]] ; then
-    export PICO_SDK_PATH="${HOME}"/projects/pico/pico-sdk
+# export PICO_SDK_PATH=/home/pi/pico/pico-sdk
+# export PICO_EXAMPLES_PATH=/home/pi/pico/pico-examples
+# export PICO_EXTRAS_PATH=/home/pi/pico/pico-extras
+# export PICO_PLAYGROUND_PATH=/home/pi/pico/pico-playground
+if [[ -d "${HOME}"/pico/pico-sdk ]] ; then
+    export PICO_SDK_PATH="${HOME}"/pico/pico-sdk
 elif [[ -d "${HOME}"/pico/pico-sdk ]] ; then
     export PICO_SDK_PATH="${HOME}"/pico/pico-sdk
 fi
 
-if [[ -d "${HOME}"/projects/pico/pico-examples ]] ; then
-    export PICO_EXAMPLES_PATH="${HOME}"/projects/pico/pico-examples
+if [[ -d "${HOME}"/pico/pico-examples ]] ; then
+    export PICO_EXAMPLES_PATH="${HOME}"/pico/pico-examples
 elif [[ -d "${HOME}"/pico/pico-examples ]] ; then
     export PICO_EXAMPLES_PATH="${HOME}"/pico/pico-examples
 fi
 
-if [[ -d "${HOME}"/projects/pico/pico-extras ]] ; then
-    export PICO_EXTRAS_PATH="${HOME}"/projects/pico/pico-extras
+if [[ -d "${HOME}"/pico/pico-extras ]] ; then
+    export PICO_EXTRAS_PATH="${HOME}"/pico/pico-extras
 elif [[ -d "${HOME}"/pico/pico-extras ]] ; then
     export PICO_EXTRAS_PATH="${HOME}"/pico/pico-extras
 fi
 
-if [[ -d "${HOME}"/projects/pico/pico-playground ]] ; then
-    export PICO_PLAYGROUND_PATH="${HOME}"/projects/pico/pico-playground
+if [[ -d "${HOME}"/pico/pico-playground ]] ; then
+    export PICO_PLAYGROUND_PATH="${HOME}"/pico/pico-playground
 elif [[ -d "${HOME}"/pico/pico-playground ]] ; then
     export PICO_PLAYGROUND_PATH="${HOME}"/pico/pico-playground
 fi
