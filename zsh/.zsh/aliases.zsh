07edef7de70b270fe3881e3bb581240559c9d30a
David Crook <idcrook@users.noreply.github.com>
2025-02-16 20:01:07 -0700

    some helpers for a miniconda PATH problem I was seeing

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -87,9 +87,27 @@ function ec () {
 # per host environment variables
 nodename=$(uname -n)
 if [[ -f "${shell_config}"/alias.$nodename.sh ]] ; then
-  # shellcheck source=/dev/null
-  source "${shell_config}"/alias.$nodename.sh
+    # shellcheck source=/dev/null
+    source "${shell_config}"/alias.$nodename.sh
 fi
+
+# some helpers for a problem I am seeing
+function conda_enter () {
+    # make sure our conda venv is at the begining of path
+    if [ -d $CONDA_PREFIX/bin ] ; then
+        path=( $CONDA_PREFIX/bin  $path )
+    fi
+}
+
+function conda_exit () {
+    # leave virtual env
+    conda deactivate     2>/dev/null || :
+
+    # https://www.reddit.com/r/zsh/comments/d7h7gb/how_to_remove_unwanted_path_entries/
+    # echo $PATH
+    # path=( ${path[@]:#*${CONDA_DEFAULT_ENV}/bin} )
+}
+
 
 
 # Navigation -------------------------------------------------------
